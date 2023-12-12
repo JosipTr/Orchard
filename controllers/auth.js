1e54f725dc0649f9776e48820e7af989ddbfc99a
JosipTr <jtr.other13@gmail.com>
2023-12-12 09:29:37 +0100

    Add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,38 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.cookies.jwt);
   res.render("auth/login", { title: "Login" });
 };
 
 exports.getRegister = (req, res, next) => {
   res.render("auth/register", { title: "Register" });
 };
+
+exports.postLogin = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const data = {
+    email: email,
+    password: password,
+  };
+  try {
+    const response = await fetch("http://localhost:8080/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.status === 200) {
+      const jsonData = await response.json();
+      res.cookie("jwt", jsonData.token);
+      return res.redirect("/");
+    }
+    res.redirect("/");
+  } catch (err) {
+    next(new Error(err));
+  }
+}
 
 exports.postRegister = async (req, res, next) => {
   const email = req.body.email;
@@ -31,7 +58,7 @@ exports.postRegister = async (req, res, next) => {
       res.cookie("jwt", jsonData.token);
       return res.redirect("/");
     }
-    res.redirect("/bla");
+    res.redirect("/");
   } catch (err) {
     next(new Error(err));
   }
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ router.get("/login", controller.getLogin);
 router.get("/register", controller.getRegister);
 router.get("/logout", controller.getLogout);
 
+router.post("/login", controller.postLogin);
 router.post("/register", controller.postRegister);
 
 module.exports = router;
